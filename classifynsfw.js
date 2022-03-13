3424201d248597844b62bd9de10d754aaef8beff
Alex-1701 <alex17koleda@gmail.com>
2022-03-13 16:59:53 +0300

    filed index

--- classifynsfw.js
+++ classifynsfw.js
@@ -5,7 +5,6 @@ const tf = require('@tensorflow/tfjs-node');
 const nsfw = require('nsfwjs');
 // const { performance } = require('perf_hooks');
 
-
 // Get Predictions by link.
 const classifyNSFWByLink = async (link) => {
     const pic = await axios.get(link, {
@@ -18,23 +17,22 @@ const classifyNSFWByLink = async (link) => {
     const image = await tf.node.decodeImage(pic.data, 3);
     const predictions = await model.classify(image);
     image.dispose(); // Tensor memory must be managed explicitly (it is not sufficient to let a tf.Tensor go out of scope for its memory to be released).
+    tf.dispose();
     // console.log(predictions);
     return predictions;
 }
 
 // Get Predictions by link.
 const classifyNSFWByFile = async (file) => {
-    const model = await nsfw.load('file://./models/quant_mid/', { type: 'graph' }); // To load a local model, nsfw.load('file://./path/to/model/')
-    // const model = nsfw.load(); // To load a local model, nsfw.load('file://./path/to/model/')
-    // Image must be in tf.tensor3d format
-    // you can convert image to tf.tensor3d with tf.node.decodeImage(Uint8Array,channels)
-
-    const image = await tf.node.decodeImage(new Uint8Array(file), 3);
-
-    const predictions = await model.classify(image);
-    image.dispose(); // Tensor memory must be managed explicitly (it is not sufficient to let a tf.Tensor go out of scope for its memory to be released).
-    // console.log(predictions);
-    return predictions;
+    try {
+        const model = await nsfw.load('file://./models/quant_mid/', { type: 'graph' });
+        const image = await tf.node.decodeImage(new Uint8Array(file), 3);
+        const predictions = await model.classify(image);
+        await image.dispose();
+        return predictions;
+    } catch {
+        return null;
+    }
 }
 
 // Calculate NSFW coefficient by link.
@@ -52,11 +50,13 @@ const analyzeLink = async (link, categories = ['Sexy', 'Porn']) => {
 // Calculate NSFW coefficient by file.
 const analyzeFile = async (file, categories = ['Sexy', 'Porn']) => {
     return classifyNSFWByFile(file).then((predictions) => {
-        const probabilities = [];
-        predictions.map(e => {
-            if (categories.includes(e.className))
-                probabilities.push(e.probability);
-        })
+        const probabilities = [0, 0];
+        if (predictions !== null) {
+            predictions.map(e => {
+                if (categories.includes(e.className))
+                    probabilities.push(e.probability);
+            })
+        }
         return Math.max(...probabilities);;
     })
 }
@@ -73,4 +73,5 @@ module.exports = { analyzeLink, analyzeFile };
 // })
 // const t1 = performance.now();
 // console.log(t1 - t0, 'milliseconds');
+
 
--- fillIndex.js
+++ fillIndex.js
@@ -2,19 +2,85 @@
 
 const fs = require('fs');
 const { analyzeFile } = require('./classifyNSFW');
+const sqlite3 = require("sqlite3").verbose();
+const baseDir = "pictures";
+const { performance } = require('perf_hooks');
+
+// 22346.1890001297 milliseconds for 100 images ~ 220 ms per image
 
 const classifyImage = async (filePath) => {
     if (fs.existsSync(filePath)) {
-        const file = fs.readFileSync(filePath);
+        let file = fs.readFileSync(filePath);
         return analyzeFile(file).then((res) => {
-            console.log(res);
+            // console.log(res);
+            file = null;
             return res;
         })
+        
     } else {
         return null;
     }
 }
 
-classifyImage('pictures/222222.png').then((res) => {
-    console.log(res);
-})
+const closeDatabase = (db) => {
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Close the database connection.");
+    });
+};
+
+const openDatabase = () => {
+    return new sqlite3.Database("registry.db", sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message)
+        }
+        console.log("open the database.")
+    })
+};
+
+const allFiles = fs.readdirSync(baseDir);
+console.log(allFiles);
+const first10Files = allFiles.slice(0, 10);
+console.log(first10Files);
+
+const db = openDatabase();
+const classifyAllImages = async (files) => {
+    return files.map(async (fileName) => {
+        const res = await classifyImage(`${baseDir}/${fileName}`);
+        const sql = `INSERT or IGNORE INTO 'index' VALUES ('${fileName.slice(0, -4)}', ${parseFloat((res).toFixed(2)) * 100})`;
+        console.log(sql);
+        try {
+            db.run(sql);
+        } catch {
+            console.log('error', sql);
+        }
+    });
+}
+
+classifyAllImages(first10Files).then(() => {
+    // closeDatabase(db);
+});
+
+// const t0 = performance.now();
+// classifyAllImages(first10Files).then(res => {
+//     console.log(res);
+//     console.log(res.length);
+//     // const t1 = performance.now();
+//     console.log(t1 - t0, 'milliseconds');
+
+
+//     const db = openDatabase();
+//     res.forEach((e) => {
+//         const sql = `INSERT INTO 'index' VALUES ('${e.name.slice(0, -4)}', ${parseFloat((e.probability).toFixed(2)) * 100})`;
+//         console.log(sql);
+//         db.run(sql);
+//     })
+
+//     closeDatabase(db);
+// });
+
+// classifyImage('pictures/222222.png').then((res) => {
+//     console.log(res);
+// })
